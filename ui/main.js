0e4f646325004b35419ad69259ab2e1af7c0d8b9
SowmeyarajanGJ <sowmeyarajangj@gmail.com>
2016-10-11 17:16:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 var element = document.getElementById ('main text');
 
 element.InnerHTML = 'New Value';
+
+// Move the image
+var img = docment.getElementById ('img');
+img.onclick = function () {
+  img.style.marginLeft = '100px';
+  
+};
