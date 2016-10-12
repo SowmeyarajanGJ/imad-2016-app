1b2648caf7a0f7acd1d0bb1ff6c6a0b5aa0f7a45
SowmeyarajanGJ <sowmeyarajangj@gmail.com>
2016-10-12 16:11:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var namee = [];
+var names = [];
 app.get('/submit-name/:name', function (req,res) {
    // Get the name from the request
    var name = req.params.name;
